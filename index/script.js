39793c3fe123b8454587ce1de17a5d763436c140
h0olm <elliot.holm02@outlook.com>
2022-11-01 04:56:36 +0100

    Remade everything

--- index/script.js
+++ index/script.js
@@ -1,14 +1,30 @@
-let slideIndex = 0;
-showSlides();
+var itemAddIcon = document.getElementsByClassName("card-item-add");
+var shoppingCartIcon = document.getElementById("shopping-cart-icon");
+var mouseClickSound = new Audio();
+mouseClickSound.src = "/audio/mouseclick.mp3"
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 10000); // Change image every 2 seconds
-}
+itemAddIcon.addEventListener("click", () => {
+    alert("Hello! I am an alert box!!");
+    shoppingCartIcon.classList.add("itemAdd");
+    if (shoppingCartIcon.className == "itemAdd") {
+
+        setTimeout(() => { shoppingCartIcon.classList.add("itemAdded"); }, 200);
+        setTimeout(() => { shoppingCartIcon.classList.remove("itemAdded", "itemAdd"); }, 300);
+    }
+})
+
+
+//var itemAddIcon = document.getElementById("item-add-icon");
+//var shoppingCartIcon = document.getElementById("shopping-cart-icon");
+//var mouseClickSound = new Audio();
+//mouseClickSound.src = "/audio/mouseclick.mp3"
+
+//itemAddIcon.addEventListener("click", () => {
+//   shoppingCartIcon.classList.toggle("itemAdd");
+//     if(shoppingCartIcon.className == "itemAdd"){
+
+//       setTimeout(() => { shoppingCartIcon.classList.toggle("itemAdded"); }, 200);
+//      setTimeout(() => { shoppingCartIcon.classList.remove("itemAdded", "itemAdd" ); }, 300);
+//   }
+//})
+
